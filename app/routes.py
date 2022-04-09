6bfdb7f5c009a5a98b80290416060033d32b6b39
joshnguyen <joshpnguyen@gmail.com>
2022-04-09 14:57:56 -0700

    Added templates, base.html being the base format for all pages and index.html.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Stephanie'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Mission Viejo!',
+            'date': '04-04-2022'
+        },
+        {
+            'author': {'username': 'Billy'},
+            'body': 'The Avengers movie was so good!!',
+            'date': '04-06-2022'
+        }
+    ]
+    return render_template('index.html', title='', user=user, posts=posts)
 
 
 app.run()
