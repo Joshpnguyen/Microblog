62823890e65c21d92c918871f6ad066d7aa40b96
Joshpnguyen <Joshpnguyen@gmail.com>
2022-04-11 20:05:01 -0700

    Added user account database.

---
+++ app/models.py
@@ -0,0 +1,13 @@
+""" User account database model (username, email, password) """
+
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
